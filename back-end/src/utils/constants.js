90e48be103f6a713de0e33e00965eec732248567
yeusone98 <tanvuong222@gmail.com>
2025-01-16 15:43:11 +0700

    Update constants.js

--- back-end/src/utils/constants.js
+++ back-end/src/utils/constants.js
@@ -1,6 +1,5 @@
 import { env } from '~/config/environment'
 export const WHITELIST_DOMAINS = [
-    'http://localhost:5173',
     'https://dv-smartphone-website.onrender.com'
 
     //.. Ví dụ sau này deploy lên domain chính thức ...vv
