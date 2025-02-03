ef6ff4fbf5ef90b4dbed380fd88a487813e95b43
yeusone98 <tanvuong222@gmail.com>
2025-02-03 11:00:53 +0700

    update API root

--- back-end/src/utils/constants.js
+++ back-end/src/utils/constants.js
@@ -1,6 +1,6 @@
 import { env } from '~/config/environment'
 export const WHITELIST_DOMAINS = [
-    'https://dv-smart-phone-website.vercel.app'
+    'https://dv-smart-phone-website.vercel.app/'
 ]
 
 export const BOARD_TYPES = {
--- front-end/src/utils/constants.js
+++ front-end/src/utils/constants.js
@@ -1,1 +1,13 @@
-export const API_ROOT = 'http://localhost:8017'
+let apiRoot = '';
+
+console.log('process.env:', process.env);
+
+if (process.env.REACT_APP_BUILD_MODE === 'dev') {
+    apiRoot = process.env.REACT_APP_API_DEV;
+} else if (process.env.REACT_APP_BUILD_MODE === 'production') {
+    apiRoot = process.env.REACT_APP_API_PRODUCTION;
+}
+
+console.log('🚀 ~ file: constants.js ~ apiRoot:', apiRoot);
+
+export const API_ROOT = apiRoot;
