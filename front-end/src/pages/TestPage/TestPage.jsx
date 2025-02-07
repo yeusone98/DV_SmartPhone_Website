835fa75bde88dee41757a828b98933129a607a86
huynhvande68 <dewippro2013@gmail.com>
2025-02-07 09:26:37 +0700

    update payment page

--- front-end/src/pages/TestPage/TestPage.jsx
+++ front-end/src/pages/TestPage/TestPage.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import TestComponent from '../../components/TestComponent/TestComponent'
 import AdminDashboard from '../../components/TestComponent/TestComponent'
+import CheckoutPage from '../../components/PaymentComponent/PaymentComponent'
 
 function TestPage() {
   return (
     <div>
-      <AdminDashboard/>
+      test
     </div>
   )
 }
